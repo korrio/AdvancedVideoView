5a5064507d8cc98388353dfd1e80c02ac48206a3
Thananon Ngoenthaworn <i.am.not.korr@gmail.com>
2015-06-17 01:33:27 +0700

    merge with AdvancedVideoView

--- sample/src/main/java/com/danikula/videocache/sample/VideoActivity.java
+++ sample/src/main/java/com/danikula/videocache/sample/VideoActivity.java
@@ -1,10 +1,23 @@
 package com.danikula.videocache.sample;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.VideoView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.danikula.videocache.Cache;
 import com.danikula.videocache.CacheListener;
@@ -12,18 +25,54 @@ import com.danikula.videocache.FileCache;
 import com.danikula.videocache.HttpProxyCache;
 import com.danikula.videocache.HttpUrlSource;
 import com.danikula.videocache.ProxyCacheException;
+import com.jit.video.DensityUtil;
+import com.jit.video.FullScreenVideoView;
+import com.jit.video.LightnessController;
+import com.jit.video.VolumnController;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class VideoActivity extends Activity implements CacheListener {
+public class VideoActivity extends Activity implements CacheListener,View.OnClickListener {
 
     private static final String LOG_TAG = "VideoActivity";
-    private static final String VIDEO_CACHE_NAME = "devbytes.mp4";
-    private static final String VIDEO_URL = "https://dl.dropboxusercontent.com/u/15506779/persistent/proxycache/devbytes.mp4";
+    private static final String VIDEO_CACHE_NAME = "X4gPz_110559_60765d771b815d6faadf2f978fb8fcfe_ori.mp4";
+    //private static final String VIDEO_URL = "https://dl.dropboxusercontent.com/u/15506779/persistent/proxycache/devbytes.mp4";
 
-    private VideoView videoView;
+    //private static final String VIDEO_URL = "http://stream-1.vdomax.com:1935/vod/__definst__/mp4:110559/110559_720p.mp4/playlist.m3u8";
+
+    private static final String VIDEO_URL ="https://www.vdomax.com/clips/2015/05/X4gPz_110559_60765d771b815d6faadf2f978fb8fcfe_ori.mp4";
+
+    //private VideoView videoView;
     private ProgressBar progressBar;
     private HttpProxyCache proxyCache;
+
+    private FullScreenVideoView mVideo;
+    private View mTopView;
+    private View mBottomView;
+    private SeekBar mSeekBar;
+    private ImageView mPlay;
+    private TextView mPlayTime;
+    private TextView mDurationTime;
+
+    private AudioManager mAudioManager;
+
+    private float width;
+    private float height;
+
+    private int playTime;
+
+    private String videoUrl = "https://www.vdomax.com/clips/2015/05/X4gPz_110559_60765d771b815d6faadf2f978fb8fcfe_ori.mp4";
+
+    private static final int HIDE_TIME = 5000;
+
+    private VolumnController volumnController;
+
+    private int orginalLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +84,32 @@ public class VideoActivity extends Activity implements CacheListener {
 
     private void setUpUi() {
         setContentView(R.layout.activity_video);
-        videoView = (VideoView) findViewById(R.id.videoView);
+        //videoView = (VideoView) findViewById(R.id.videoView);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.setMax(100);
+
+        volumnController = new VolumnController(this);
+        mVideo = (FullScreenVideoView) findViewById(R.id.videoview);
+        mPlayTime = (TextView) findViewById(R.id.play_time);
+        mDurationTime = (TextView) findViewById(R.id.total_time);
+        mPlay = (ImageView) findViewById(R.id.play_btn);
+        mSeekBar = (SeekBar) findViewById(R.id.seekbar);
+        mTopView = findViewById(R.id.top_layout);
+        mBottomView = findViewById(R.id.bottom_layout);
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        width = DensityUtil.getWidthInPx(this);
+        height = DensityUtil.getHeightInPx(this);
+        threshold = DensityUtil.dip2px(this, 18);
+
+        orginalLight = LightnessController.getLightness(this);
+
+        mPlay.setOnClickListener(this);
+        mSeekBar.setOnSeekBarChangeListener(mSeekBarChangeListener);
+
+        playVideo();
+
     }
 
     private void playWithCache() {
@@ -46,16 +118,31 @@ public class VideoActivity extends Activity implements CacheListener {
             HttpUrlSource source = new HttpUrlSource(VIDEO_URL);
             proxyCache = new HttpProxyCache(source, cache);
             proxyCache.setCacheListener(this);
-            videoView.setVideoPath(proxyCache.getUrl());
-            videoView.start();
+            mVideo.setVideoPath(proxyCache.getUrl());
+            mVideo.start();
         } catch (ProxyCacheException e) {
             // do nothing. onError() handles all errors
         }
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            height = DensityUtil.getWidthInPx(this);
+            width = DensityUtil.getHeightInPx(this);
+        } else if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            width = DensityUtil.getWidthInPx(this);
+            height = DensityUtil.getHeightInPx(this);
+        }
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
     public void onDestroy() {
         super.onDestroy();
+
+        mHandler.removeMessages(0);
+        mHandler.removeCallbacksAndMessages(null);
 
         if (proxyCache != null) {
             proxyCache.shutdown();
@@ -70,6 +157,329 @@ public class VideoActivity extends Activity implements CacheListener {
     @Override
     public void onCacheDataAvailable(int cachePercentage) {
         progressBar.setProgress(cachePercentage);
+    }
+
+    private SeekBar.OnSeekBarChangeListener mSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            mHandler.postDelayed(hideRunnable, HIDE_TIME);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            mHandler.removeCallbacks(hideRunnable);
+        }
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                                      boolean fromUser) {
+            if (fromUser) {
+                int time = progress * mVideo.getDuration() / 100;
+                mVideo.seekTo(time);
+            }
+        }
+    };
+
+    private void backward(float delataX) {
+        int current = mVideo.getCurrentPosition();
+        int backwardTime = (int) (delataX / width * mVideo.getDuration());
+        int currentTime = current - backwardTime;
+        mVideo.seekTo(currentTime);
+        mSeekBar.setProgress(currentTime * 100 / mVideo.getDuration());
+        mPlayTime.setText(formatTime(currentTime));
+    }
+
+    private void forward(float delataX) {
+        int current = mVideo.getCurrentPosition();
+        int forwardTime = (int) (delataX / width * mVideo.getDuration());
+        int currentTime = current + forwardTime;
+        mVideo.seekTo(currentTime);
+        mSeekBar.setProgress(currentTime * 100 / mVideo.getDuration());
+        mPlayTime.setText(formatTime(currentTime));
+    }
+
+    private void volumeDown(float delatY) {
+        int max = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int current = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int down = (int) (delatY / height * max * 3);
+        int volume = Math.max(current - down, 0);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+        int transformatVolume = volume * 100 / max;
+        volumnController.show(transformatVolume);
+    }
+
+    private void volumeUp(float delatY) {
+        int max = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int current = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        int up = (int) ((delatY / height) * max * 3);
+        int volume = Math.min(current + up, max);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+        int transformatVolume = volume * 100 / max;
+        volumnController.show(transformatVolume);
+    }
+
+    private void lightDown(float delatY) {
+        int down = (int) (delatY / height * 255 * 3);
+        int transformatLight = LightnessController.getLightness(this) - down;
+        LightnessController.setLightness(this, transformatLight);
+    }
+
+    private void lightUp(float delatY) {
+        int up = (int) (delatY / height * 255 * 3);
+        int transformatLight = LightnessController.getLightness(this) + up;
+        LightnessController.setLightness(this, transformatLight);
+    }
+
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    if (mVideo.getCurrentPosition() > 0) {
+                        mPlayTime.setText(formatTime(mVideo.getCurrentPosition()));
+                        int progress = mVideo.getCurrentPosition() * 100 / mVideo.getDuration();
+                        mSeekBar.setProgress(progress);
+                        if (mVideo.getCurrentPosition() > mVideo.getDuration() - 100) {
+                            mPlayTime.setText("00:00");
+                            mSeekBar.setProgress(0);
+                        }
+                        mSeekBar.setSecondaryProgress(mVideo.getBufferPercentage());
+                    } else {
+                        mPlayTime.setText("00:00");
+                        mSeekBar.setProgress(0);
+                    }
+
+                    break;
+                case 2:
+                    showOrHide();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    };
+
+
+    private void playVideo() {
+        mVideo.setVideoPath(videoUrl);
+        mVideo.requestFocus();
+        mVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mVideo.setVideoWidth(mp.getVideoWidth());
+                mVideo.setVideoHeight(mp.getVideoHeight());
+
+                mVideo.start();
+                if (playTime != 0) {
+                    mVideo.seekTo(playTime);
+                }
+
+                mHandler.removeCallbacks(hideRunnable);
+                mHandler.postDelayed(hideRunnable, HIDE_TIME);
+                mDurationTime.setText(formatTime(mVideo.getDuration()));
+                Timer timer = new Timer();
+                timer.schedule(new TimerTask() {
+
+                    @Override
+                    public void run() {
+                        mHandler.sendEmptyMessage(1);
+                    }
+                }, 0, 1000);
+            }
+        });
+        mVideo.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mPlay.setImageResource(R.drawable.video_btn_down);
+                mPlayTime.setText("00:00");
+                mSeekBar.setProgress(0);
+            }
+        });
+        mVideo.setOnTouchListener(mTouchListener);
+    }
+
+    private Runnable hideRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            showOrHide();
+        }
+    };
+
+    @SuppressLint("SimpleDateFormat")
+    private String formatTime(long time) {
+        DateFormat formatter = new SimpleDateFormat("mm:ss");
+        return formatter.format(new Date(time));
+    }
+
+    private float mLastMotionX;
+    private float mLastMotionY;
+    private int startX;
+    private int startY;
+    private int threshold;
+    private boolean isClick = true;
+
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            final float x = event.getX();
+            final float y = event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    mLastMotionX = x;
+                    mLastMotionY = y;
+                    startX = (int) x;
+                    startY = (int) y;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    float deltaX = x - mLastMotionX;
+                    float deltaY = y - mLastMotionY;
+                    float absDeltaX = Math.abs(deltaX);
+                    float absDeltaY = Math.abs(deltaY);
+                    // 声音调节标识
+                    boolean isAdjustAudio = false;
+                    if (absDeltaX > threshold && absDeltaY > threshold) {
+                        if (absDeltaX < absDeltaY) {
+                            isAdjustAudio = true;
+                        } else {
+                            isAdjustAudio = false;
+                        }
+                    } else if (absDeltaX < threshold && absDeltaY > threshold) {
+                        isAdjustAudio = true;
+                    } else if (absDeltaX > threshold && absDeltaY < threshold) {
+                        isAdjustAudio = false;
+                    } else {
+                        return true;
+                    }
+                    if (isAdjustAudio) {
+                        if (x < width / 2) {
+                            if (deltaY > 0) {
+                                lightDown(absDeltaY);
+                            } else if (deltaY < 0) {
+                                lightUp(absDeltaY);
+                            }
+                        } else {
+                            if (deltaY > 0) {
+                                volumeDown(absDeltaY);
+                            } else if (deltaY < 0) {
+                                volumeUp(absDeltaY);
+                            }
+                        }
+
+                    } else {
+                        if (deltaX > 0) {
+                            forward(absDeltaX);
+                        } else if (deltaX < 0) {
+                            backward(absDeltaX);
+                        }
+                    }
+                    mLastMotionX = x;
+                    mLastMotionY = y;
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (Math.abs(x - startX) > threshold
+                            || Math.abs(y - startY) > threshold) {
+                        isClick = false;
+                    }
+                    mLastMotionX = 0;
+                    mLastMotionY = 0;
+                    startX = (int) 0;
+                    if (isClick) {
+                        showOrHide();
+                    }
+                    isClick = true;
+                    break;
+
+                default:
+                    break;
+            }
+            return true;
+        }
+
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play_btn:
+                if (mVideo.isPlaying()) {
+                    mVideo.pause();
+                    mPlay.setImageResource(R.drawable.video_btn_down);
+                } else {
+                    mVideo.start();
+                    mPlay.setImageResource(R.drawable.video_btn_on);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showOrHide() {
+        if (mTopView.getVisibility() == View.VISIBLE) {
+            mTopView.clearAnimation();
+            Animation animation = AnimationUtils.loadAnimation(this,
+                    R.anim.option_leave_from_top);
+            animation.setAnimationListener(new AnimationImp() {
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    super.onAnimationEnd(animation);
+                    mTopView.setVisibility(View.GONE);
+                }
+            });
+            mTopView.startAnimation(animation);
+
+            mBottomView.clearAnimation();
+            Animation animation1 = AnimationUtils.loadAnimation(this,
+                    R.anim.option_leave_from_bottom);
+            animation1.setAnimationListener(new AnimationImp() {
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    super.onAnimationEnd(animation);
+                    mBottomView.setVisibility(View.GONE);
+                }
+            });
+            mBottomView.startAnimation(animation1);
+        } else {
+            mTopView.setVisibility(View.VISIBLE);
+            mTopView.clearAnimation();
+            Animation animation = AnimationUtils.loadAnimation(this,
+                    R.anim.option_entry_from_top);
+            mTopView.startAnimation(animation);
+
+            mBottomView.setVisibility(View.VISIBLE);
+            mBottomView.clearAnimation();
+            Animation animation1 = AnimationUtils.loadAnimation(this,
+                    R.anim.option_entry_from_bottom);
+            mBottomView.startAnimation(animation1);
+            mHandler.removeCallbacks(hideRunnable);
+            mHandler.postDelayed(hideRunnable, HIDE_TIME);
+        }
+    }
+
+    private class AnimationImp implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+
     }
 
 }
